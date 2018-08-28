e816262f14a12c3b2320f3f57f4e6cd56de1380b
Kostiantyn Bovt <kbovt@e1r1p11.unit.ua>
2018-08-28 15:52:03 +0300

    db_dump

--- backend/test2.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-for k in range(1, 30):
-	geo_lat = float(randint(50315498066384631,50521129770500394)) / float(1000000000000000)
-	geo_lng = float(randint(30257072037908188,30676172560903183)) / float(1000000000000000)
-
-	print geo_lat
-	print geo_lng
-	print
